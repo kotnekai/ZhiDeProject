22a36e9da65360a172d13691cb6a3c9fe36db9ca
huth3 <huth3@meicloud.com>
2018-10-23 16:37:42 +0800

    关于我们

--- app/src/main/java/com/zhide/app/view/activity/AboutActivity.java
+++ app/src/main/java/com/zhide/app/view/activity/AboutActivity.java
@@ -2,36 +2,22 @@ package com.zhide.app.view.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Paint;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 import com.zhide.app.R;
-import com.zhide.app.common.ComApplication;
-import com.zhide.app.common.CommonParams;
-import com.zhide.app.eventBus.LoginEvent;
-import com.zhide.app.logic.LogicManager;
-import com.zhide.app.model.RegisterLoginModel;
-import com.zhide.app.utils.PreferencesUtils;
-import com.zhide.app.utils.ProgressUtils;
-import com.zhide.app.utils.ToastUtil;
+import com.zhide.app.common.ApplicationHolder;
 import com.zhide.app.utils.UIUtils;
 import com.zhide.app.view.base.BaseActivity;
 
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
-
 import butterknife.BindView;
-import butterknife.OnClick;
 
 public class AboutActivity extends BaseActivity {
 
+
+    @BindView(R.id.tvVersion)
+    TextView tvVersion;
 
     public static Intent makeIntent(Context context) {
         Intent intent = new Intent(context, AboutActivity.class);
@@ -60,7 +46,9 @@ public class AboutActivity extends BaseActivity {
     }
 
     private void initView() {
+       String version = ApplicationHolder.getInstance().getVersionModel().getVersionName();
 
+        tvVersion.setText(String.format(getString(R.string.depart_name),version));
     }
 
 }
