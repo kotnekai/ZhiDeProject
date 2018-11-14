0945d03a6471503578a03ed8f1a7891a9377e6ca
许运昌 <xuyunchang@mzjmedia.net>
2018-11-14 17:34:45 +0800

    退款二次确认弹窗

--- app/src/main/java/com/zhide/app/view/activity/WithdrawActivity.java
+++ app/src/main/java/com/zhide/app/view/activity/WithdrawActivity.java
@@ -16,6 +16,7 @@ import com.zhide.app.logic.UserManager;
 import com.zhide.app.model.ResponseModel;
 import com.zhide.app.model.UserData;
 import com.zhide.app.model.WithdrawModel;
+import com.zhide.app.utils.DialogUtils;
 import com.zhide.app.utils.EmptyUtil;
 import com.zhide.app.utils.PreferencesUtils;
 import com.zhide.app.utils.ToastUtil;
@@ -116,27 +117,33 @@ public class WithdrawActivity extends BaseActivity{
 
 
     private void submitOrder() {
-        long userId = PreferencesUtils.getLong(CommonParams.LOGIN_USER_ID);
-        String payeeName = edtPayeeName.getText().toString();
-        String acceptAccount = edtAcceptCount.getText().toString();
-        String phoneNumber = edtPhoneNumber.getText().toString();
+        final long userId = PreferencesUtils.getLong(CommonParams.LOGIN_USER_ID);
+        final String payeeName = edtPayeeName.getText().toString();
+        final String acceptAccount = edtAcceptCount.getText().toString();
+        final String phoneNumber = edtPhoneNumber.getText().toString();
         String surplusMoney = tvSurplusMoney.getText().toString();
         if (EmptyUtil.isEmpty(payeeName) || EmptyUtil.isEmpty(acceptAccount) || EmptyUtil.isEmpty(phoneNumber)) {
             ToastUtil.showShort(getString(R.string.with_draw_empty_tip));
             return;
         }
-        float withdrawMoney = Float.parseFloat(surplusMoney);
+        final float withdrawMoney = Float.parseFloat(surplusMoney);
         if (withdrawMoney == 0) {
             ToastUtil.showShort(getString(R.string.can_withdraw_money));
             return;
         }
-        WithdrawModel params = new WithdrawModel();
-        params.setUSW_Money(withdrawMoney);
-        params.setUSI_Id(userId);
-        params.setUSW_AccountName(payeeName);
-        params.setUSW_Account(acceptAccount);
-        params.setUSW_ContactMobile(phoneNumber);
-        UserManager.getInstance().doWithdraw(params);
+        DialogUtils.showConfirmDialog(this, "是否确认退款？", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WithdrawModel params = new WithdrawModel();
+                params.setUSW_Money(withdrawMoney);
+                params.setUSI_Id(userId);
+                params.setUSW_AccountName(payeeName);
+                params.setUSW_Account(acceptAccount);
+                params.setUSW_ContactMobile(phoneNumber);
+                UserManager.getInstance().doWithdraw(params);
+            }
+        });
+
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
